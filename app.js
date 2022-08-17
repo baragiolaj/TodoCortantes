a86a8c41bf8de85e76bb94f10b24779ea0fa0b7c
Naiara <juanchininaiara@gmail.com>
2022-08-16 22:37:35 -0300

    Enlaces header

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     res.render('pages/index');
-});
-
-app.get('/categorias', function(req, res) {
-    res.render('pages/categorias');
 });
 
 app.get('/contacto', function(req, res) {
@@ -19,6 +18,12 @@ app.get('/carrito', function(req, res) {
     res.render('pages/carrito');
 });
 
+app.get('/newsletter', function(req, res) {
+    res.render('pages/newsletter');
+});
+app.get('/products', function(req, res) {
+    res.render('pages/products');
+});
 
 app.listen(3000);
 console.log ('anda todo')
